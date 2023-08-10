18dbf9b2bb15c8f448833d368b9dfe2b15f1437b
Maxi Ovelar <73977645+maxiovelar@users.noreply.github.com>
2023-08-10 21:07:11 +0200

    Add new origin to fast api

--- api/server.py
+++ api/server.py
@@ -15,6 +15,7 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:8000",
+    "http://127.0.0.1:8000",
     "http://localhost:3000",
     "http://localhost:3001",
     "http://localhost:3002",
