3ba1f45472f936ab7aa7eee24601aa4aa0900b46
JoaquinEtchegaray <etchegarayjoaco@gmail.com>
2023-08-14 16:45:21 -0300

    test readd langchain fn

--- api/openai_related.py
+++ api/openai_related.py
@@ -6,6 +6,9 @@ from api.pinecone_db import get_db_instance
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
+import openai
+
+
 
 
 
@@ -17,47 +20,57 @@ def query_related(query):
 
     db = get_db_instance()
     
-    return print(db)
-
-    # # prompt_template = """
-    # # You will receive a object containing a product name delimited with <>. Your task is to use the following pieces of context to find a maximum of three products that are related to the product delimited with <>.
-    # # Return an array with the id of each related product.
-    # # If you can not find any related products, just return an empty array.
-    # # Don't return duplicated products.
-    # # Don't return the product provided in the final result.
-    # # Don't return an object.
-
-    # # {context}
-
-    # # Product: <{question}>
-    # # Answer in JSON format:"""
 
     # prompt_template = """
-    # You will receive a product name delimited with <>. Your task is to use the following pieces of context to find a maximum of three products that are related to the product delimited with <>. Once found, return an array with the id of each of those products.
-    # # If you can not find any related products, just return an empty array.
-    # # Don't return duplicated products.
-    # # Don't return the product provided in the final result.
-    # # Don't return an object.
-    # # Return a maximum of 3 products.
+    # You will receive a object containing a product name delimited with <>. Your task is to use the following pieces of context to find a maximum of three products that are related to the product delimited with <>.
+    # Return an array with the id of each related product.
+    # If you can not find any related products, just return an empty array.
+    # Don't return duplicated products.
+    # Don't return the product provided in the final result.
+    # Don't return an object.
 
     # {context}
 
     # Product: <{question}>
     # Answer in JSON format:"""
 
-    # PROMPT = PromptTemplate(template=prompt_template,
-    #                         input_variables=["context", "question"])
+    prompt_template = """
+    You will receive a product name delimited with <>. Your task is to use the following pieces of context to find a maximum of three products that are related to the product delimited with <>. Once found, return an array with the id of each of those products.
+    # If you can not find any related products, just return an empty array.
+    # Don't return duplicated products.
+    # Don't return the product provided in the final result.
+    # Don't return an object.
+    # Return a maximum of 3 products.
 
-    # chain_type_kwargs = {"prompt": PROMPT}
+    {context}
 
-    # qa = RetrievalQA.from_chain_type(
-    #     llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0,
-    #                    openai_api_key=os.getenv('OPENAI_API_KEY')),
-    #     chain_type="stuff",
-    #     retriever=db.as_retriever(),
-    #     chain_type_kwargs=chain_type_kwargs
+    Product: <{question}>
+    Answer in JSON format:"""
+
+    PROMPT = PromptTemplate(template=prompt_template,
+                            input_variables=["context", "question"])
+
+    chain_type_kwargs = {"prompt": PROMPT}
+
+    qa = RetrievalQA.from_chain_type(
+        llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0,
+                       openai_api_key=os.getenv('OPENAI_API_KEY')),
+        chain_type="stuff",
+        retriever=db.as_retriever(),
+        chain_type_kwargs=chain_type_kwargs
+    )
+
+    res = qa.run(query)
+    
+    
+    # chat = openai.ChatCompletion.create(
+    #         model="gpt-3.5-turbo",
+    #         messages=[
+    #             {"role": "system", "content": "You are a helpful assistant."},
+    #             {"role": "assistant", "content": "Return an array of only product names similar to the given one"},
+    #             {"role": "user", "content": query},
+    #         ]
     # )
+    
 
-    # res = qa.run(query)
-
-    # return json.loads(res)
+    # return print(chat.choices[0].message.content)
